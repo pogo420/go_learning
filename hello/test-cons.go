8c6a6794f56b5a7c2e4364346823af2565f9e6ab
Arnab Mukherjee <am0072008@gmail.com>
2021-07-26 23:36:20 +0530

    constant feature go

---
+++ hello/test-cons.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const VAR string = "KOLO))("
+const NUM int = 34
+
+
+func test_defer() {
+	fmt.Println("something in test_defer")
+	defer fmt.Println("cleanup test-defer")
+}
+
+
+
+func main() {
+
+	fmt.Println(VAR,NUM)
+	defer fmt.Println("main cleanup")
+	test_defer()
+}
+
